7763d4eebc1deaf3c94b31d09b4a800fdf080e39
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-08-10 12:35:46 -0400

    setting up server ans installing dependencies

--- frontend/src/pages/Orderpage.jsx
+++ frontend/src/pages/Orderpage.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import axios from "axios";
+import { Link } from "react-router-dom";
 function Orderpage() {
-  return (
-    <div>Orderpage</div>
-  )
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    const fecthOrder = async () => {
+      const { data } = await axios.get("/orders");
+      setOrders(data);
+    };
+    fecthOrder();
+  });
+  return <div>Orderpage</div>;
 }
 
-export default Orderpage
+export default Orderpage;
